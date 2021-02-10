2bf7a93e4d60c026c8f4305a2a24abfff31663a2
itzhuzhu <itzhuzhu@163.com>
2021-02-10 17:42:39 +0800

    第一次提交

---
+++ studentSystem/domain/test.java
@@ -0,0 +1,11 @@
+package studentSystem.domain;
+
+/**
+ * @author itzhuzhu
+ * @date 2021/2/10 5:42 下午
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("第一次");
+    }
+}
